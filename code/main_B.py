2bae2beb1b3e2bf07f2d00bbd69793d2ac12b55a
ehwgal <78798294+ehwgal@users.noreply.github.com>
2022-01-23 20:39:33 +0100

    Add files via upload

---
+++ code/main_B.py
@@ -0,0 +1,176 @@
+import sys
+import pandas as pd
+from utils_B import *
+import nltk
+from nltk import pos_tag
+from nltk.corpus import stopwords, wordnet, words, treebank
+from nltk.stem import WordNetLemmatizer
+wnl = WordNetLemmatizer()
+
+stopwords_list = stopwords.words('english')
+df = pd.read_csv('./data/SEM-2012-SharedTask-CD-SCO-training-simple.v2.txt', sep="\t", names=["story", "sent_index", "token_index", "token", "bio"])
+
+multiword_expressions = [["by", "no", "means"], ["on", "the", "contrary"], ["rather", "than"], ["not", "for", "the", "world"], ["nothing", "at", "all"], ["no", "more"]]
+
+negation_stopwords_set = ["nobody", "neither", "nowhere", "not", "no", "without", "nevertheless", "nor", "never", "ain", "aren", "aren't", 'couldn', "couldn't", "didn", "didn't", "doesn", "doesn't", \
+"hadn", "hadn't", "hasn", "hasn't", "haven't","isn", "isn't", "mightn", "mightn't", "mustn", "mustn't", "needn", "needn't", "shan", "shan't", "shouldn", "shouldn't", "wasn", "wasn't",\
+ "weren", "weren't", "won't", "wouldn", "wouldn't", "don't", "'t"]
+
+# filtering out negations that are in the NLTK stopwords list
+new_stopwords_list = []
+for i in stopwords_list:
+    if i not in negation_stopwords_set:
+        new_stopwords_list.append(i)
+
+prefixes = {"un", "dis", "in", "a", "ab", "an", "non-", "im", "il", "ir", "anti"}
+suffixes = {"less", "lessly", "lessness"}
+
+def lemmatize_tokens(df):
+    """Input dataframe. Creates lemmas for lemma column and uses NLTK lemmatizer."""
+    return df.apply((lemma_wrapper),axis=1)
+
+def part_of_speech(df):
+    """Input dataframe. Creates PoS tags for column in df. NLTK PoS-tagger is used"""
+    token_list = df['token'].tolist()
+    pos_token_list = pos_tag(token_list)
+    pos_list = [tuple[1] for tuple in pos_token_list]
+    return pos_list
+
+def matches_neg_exp(negation_stopwords_set,df):
+    """Input dataframe. Creates booleans for MatchesNeg-column in the dataframe. Checks for lexical negations"""
+    return df['token'].apply(lambda x: x.lower() in negation_stopwords_set)
+    
+def element_has_prefix(token):
+    """Checks whether a token contains a negation prefix. Inputs token, returns boolean."""
+    prefixes_list = []
+    token = token.lower()
+    if check_prefix(token):
+        if check_negation_prefix(token):
+            return True
+        else:
+            return False
+    else:
+        return False
+
+def has_prefix(df):
+    """Input dataframe. Creates booleans for hasPrefix-column and checks for prefixal negations"""
+    return df['token'].apply(element_has_prefix)
+
+def element_has_suffix(token):
+    """Checks whether a token contains a negation suffix. Inputs token, returns boolean."""
+    suffixes_list = []
+    token = token.lower()
+    if check_suffix(token):
+        if check_negation_suffix(token):
+            return True
+        else:
+            return False
+    else:
+        return False
+    
+def has_suffix(suffixes):
+    """Inputs dataframe. Creates booleans for hasSuffix-column and checks for suffixal negations"""
+    return df['token'].apply(element_has_suffix)
+
+def has_prefix_and_antonym(df):
+    """Inputs dataframe. Creates booleans for hasPrefix&Antonym-column and checks for prefixal negations using WordNet antonyms"""
+    return df.apply((check_antonym_prefix_wrapper), axis=1)
+
+def has_suffix_and_antonym(df):
+    """Inputs dataframe. Creates booleans for hasSuffix&Antonym-column and checks for suffixal negations using Wordnet antonyms"""
+    return df.apply((check_antonym_suffix_wrapper), axis=1)
+
+def get_multiword(mwe_list, df):
+    """Input dataframe and multiword expressions list.
+    Creates matchesMulticue column in dataframe containing boolean values.
+    Sets all multicue tokens to True.""" 
+    # setting every value to False
+    df['matchesMulticue'] = [False for x in range(len(df))]
+    
+    for i in range(len(df)):
+        for exp in mwe_list:
+            is_match = True
+            # ensure index doesn't go outside dataframe
+            if i + len(exp) >= len(df):
+                return
+            # check if token matches first word in exp or not
+            if df.iloc[i]['token'] != exp[0]:
+                # if not, continue to next token
+                continue
+            # otherwise, check if full exp matches
+            for j in range(len(exp)):
+                if exp[j] != df.iloc[i+j]['token']:
+                    is_match = False
+                    break
+            # if it's a match, then change to True in dataframe
+            if is_match:
+                for j in range(len(exp)):
+                    df.at[i + j, 'matchesMulticue'] =  True
+                i = (i + len(exp)) - 1
+                continue
+
+def chunking(df):
+    """Identifies the chunk that each token is a part of, based on the PoS Treebank tags.
+    Input is dataframe and output is a list of chunk labels."""
+    # Based on NLTK's RegexpParser tutorial: https://www.nltk.org/book_1ed/ch07.html
+    grammar = r"""
+    NP: {<WP|WDT|WRB>?<DT|PRP.*>?<JJ.*>?<NN.*|PRP.*>+} # noun phrase
+    PP: {<IN><NP>}                                     # prepositional phrase
+    VP: {<MD>?<VB.*>?<VB.*><RB.*>?<NP|PP|CLAUSE>+}    # verb phrase
+    CLAUSE: {<NP><VP>}                                 # clause
+    ADJP: {<RB.*>?<RB.*>?<JJ.*>}                       # adjectival phrase
+    ADVP: {<RB.*><VB.*|JJ.*|NN.*>}                     # adverbial phrase
+    """
+    ###
+    tokens = df["token"].tolist()
+    # retagging as the chunker requires tuples
+    pos = pos_tag(tokens)
+    chunkParser = nltk.RegexpParser(grammar)
+    tree = chunkParser.parse(pos)
+    labels = getNodes(tree)
+    return labels
+
+def main(argv=None):
+    """Creates dataframe containing all features.
+    This dataframe is written to a .csv file."""
+    
+    if argv is None:
+        argv = sys.argv
+    
+    argv = ['','./../data/SEM-2012-SharedTask-CD-SCO-training-simple.v2.txt']
+    trainingfile = argv[1]
+    
+    df = pd.read_csv(trainingfile, sep="\t", names=["story", "sent_index", "token_index", "token", "bio"])
+    
+    # some of the features are currently quite similar,
+    # we will make a selection when building the classifier
+    
+    df['previous_token'] = df['token'].shift(1)
+    df['next_token'] = df['token'].shift(-1)
+    df['pos'] = part_of_speech(df)
+    
+    # need to convert to WordNet PoS tags for the following functions
+    df["pos"] = df["pos"].apply(get_wordnet_pos)
+    df['hasPrefixAntonym'] = has_prefix_and_antonym(df)
+    df['hasSuffixAntonym'] = has_suffix_and_antonym(df)
+    df['lemma'] = lemmatize_tokens(df)
+    
+    # changing column back to original Treebank PoS tags
+    df['pos'] = part_of_speech(df)
+    df['chunk'] = chunking(df)
+    
+    df['matchesNeg'] = matches_neg_exp(negation_stopwords_set, df)
+    df['hasPrefix'] = has_prefix(df)
+    df['hasSuffix'] = has_suffix(df)
+   
+    get_multiword(multiword_expressions, df)
+    
+    #specifying the order of the dataframe
+    new_df = df[['story', 'sent_index', 'token_index', 'previous_token', 'token', 'next_token', 'pos', 'chunk', 'lemma', 'matchesNeg', 'hasPrefix', 'hasSuffix', 'hasPrefixAntonym', 'hasSuffixAntonym', 'matchesMulticue', 'bio']]
+    
+    csvfile = 'features_B.csv'
+    new_df.to_csv(csvfile)
+
+
+if __name__ == '__main__':
+    main()
---
+++ code/utils_A.py
@@ -0,0 +1,163 @@
+import nltk
+from nltk import pos_tag
+import pandas as pd
+import spacy
+from utils_A import *
+from nltk.corpus import stopwords, wordnet, words, treebank
+from spacy.lang.en.stop_words import STOP_WORDS
+
+nlp = spacy.load("en_core_web_sm", disable=["tokenizer", "parser","ner", "textcat", "custom"])
+df = pd.read_csv('./data/SEM-2012-SharedTask-CD-SCO-training-simple.v2.txt', sep="\t", names=["story", "sent_index", "token_index", "token", "bio"])
+
+mwe = [["by", "no", "means"], ["on", "the", "contrary"], ["rather", "than"], ["not", "for", "the", "world"], ["nothing", "at", "all"], ["no", "more"]]
+
+negation_stopwords_set = {"n't", "n’t", "n‘t","cannot", "nobody", "neither", "nowhere", "not", "no", "without", "nevertheless", "nor", "never"}
+# removing negation words from spacy stopwords set
+new_stopwords_list = []
+for i in STOP_WORDS:
+    if i not in negation_stopwords_set:
+        new_stopwords_list.append(i)
+
+prefixes = {"un", "dis", "in", "a", "ab", "an", "non-", "im", "il", "ir", "anti"}
+suffixes = {"less", "lessly", "lessness"}
+
+# TAKEN FROM: https://stackoverflow.com/questions/14841997/how-to-navigate-a-nltk-tree-tree
+def getNodes(parent):
+    """Gets chunk labels from syntactic tree"""
+    labels = []
+    for node in parent:
+        if type(node) is nltk.Tree:
+            for leave in node.leaves():
+                labels.append(node.label())
+        else:
+            labels.append("no label")
+    return labels
+###
+
+def spacy_tagger_wrapper(row):
+    """Wrapper for spacy pos-tag function"""
+    return spacy_tagger(str(row.token))
+
+def spacy_tagger(token):
+    """Function pos-tags token"""
+    doc = nlp(token)
+    for i in doc:
+        tag = i.pos_
+    return tag
+
+def delete_one_prefix(token, prefixes):
+    """The prefixes are deleted from an input token, using the prefixes-set. Output is the remaining token"""
+    for prefix in prefixes:
+        token = token.lower()
+        if token.startswith(prefix) and token not in new_stopwords_list:
+            token = token.strip(prefix)
+    return token
+
+def delete_one_suffix(token, suffixes):
+    """The suffixes are deleted from an input token, using the suffixes-set. Output is the remaining token"""
+    for suffix in suffixes:
+        token = token.lower()
+        if token.endswith(suffix) or token not in new_stopwords_list:
+            token = token.strip(suffix)
+    return token
+
+def lemma_wrapper(row):
+    """Wrapper for lemmatizer function"""
+    return spacy_lemmatizer_token(str(row.token))
+
+def spacy_lemmatizer_token(token):
+    """One input token is lemmatized using SpaCy. The lemma is returned"""
+    doc = nlp(token)
+    for i in doc:
+        lemma = i.lemma_
+    return lemma
+
+def find_antonyms_wrapper(row):
+    """Wrapper for find_antonyms function"""
+    return find_antonyms(str(row.token))
+
+def check_antonym_prefix_wrapper(row):
+    """Wrapper for check_antonym_is_token_prefix function"""
+    return check_antonym_is_token_prefix(str(row.token))
+
+def check_antonym_suffix_wrapper(row):
+    """Wrapper for check_antonym_is_token_suffix function"""
+    return check_antonym_is_token_suffix(str(row.token))
+
+def find_antonyms(token):
+    """Finds antonyms of an input token with help of input PoS tag, returns list of antonyms"""
+    lemma = spacy_lemmatizer_token(token)
+    antonyms_list = []
+    for syn in wordnet.synsets(lemma):
+        for l in syn.lemmas():
+            if l.antonyms():
+                antonyms_list.append(l.antonyms()[0].name())
+    return antonyms_list
+
+def check_antonym_is_token_prefix(token):
+    """Checks whether antonym is a token without the prefix.
+    Input token and PoS-tag, outputs boolean."""
+    antonyms_list = find_antonyms(token)
+    if len(antonyms_list) == 0:
+        return False
+    for prefix in prefixes:
+        if token.startswith(prefix):
+            new_token = token.strip(prefix)
+            for antonym in antonyms_list:
+                if new_token == antonym:
+                    return True
+    return False
+
+def check_antonym_is_token_suffix(token):
+    """Checks whether antonym is a token without the suffix.
+    Input token and PoS-tag, outputs boolean."""
+    antonyms_list = find_antonyms(token)
+    if len(antonyms_list) == 0:
+        return False
+    for suffix in suffixes:
+        if token.endswith(suffix):
+            new_token = token.strip(suffix)
+            for antonym in antonyms_list:
+                if new_token == antonym:
+                    return True
+    return False
+
+def check_prefix(token):
+    """Checks whether a prefix is in a token.
+    Inputs token, outputs boolean."""
+    token = token.lower()
+    for prefix in prefixes:
+        if token.startswith(prefix) and token != prefix  and token not in new_stopwords_list:
+            return True
+    return False
+
+def check_suffix(token):
+    """Checks whether a suffix is in a token.
+    Inputs token, outputs boolean."""
+    token = token.lower()
+    for suffix in suffixes:
+        if token.endswith(suffix) and token != suffix and token not in new_stopwords_list:
+            return True
+    return False
+
+def check_negation_prefix(token):
+    """Checks whether the prefix is a negation by looking at the remaining token.
+    Inputs token, outputs boolean."""
+    new_token = delete_one_prefix(token, prefixes)
+    boolean = check_if_word(new_token)
+    return boolean
+
+def check_negation_suffix(token):
+    """Checks whether the suffix is a negation by looking at the remaining token.
+    Inputs token, outputs boolean."""
+    new_token = delete_one_suffix(token, suffixes)
+    boolean = check_if_word(new_token)
+    return boolean
+
+def check_if_word(word):
+    """Using NLTK words corpus, a token is checked if it is a word. The output is a boolean"""
+    word = word.lower()
+    if  1 <= len(word) <= 3:
+        return False
+    else:
+        return word in words.words()
